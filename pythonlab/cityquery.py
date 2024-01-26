34a5539f3dfa3376bf74898ed27c6f7f5911b14d
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-01-26 10:36:00 -0600

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -39,7 +39,7 @@ def largestPopulation():
 
     for row in row_list:
         print(row)
-        if(row > int(largestPop)):
+        if(int(row) > int(largestPop)):
             largestPop = row
     
     query = "SELECT city_population FROM cities WHERE city_population = %s"
