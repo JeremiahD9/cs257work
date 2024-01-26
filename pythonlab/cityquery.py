cb720d9373c212bcc7731d8d7925003f29e327de
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-01-26 11:01:47 -0600

    Update cityquery.py
    Added Smallest City in MN Method

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -1,4 +1,5 @@
 import psycopg2
+import sys
 
 def isNorthfield():
     conn = psycopg2.connect(
@@ -38,7 +39,6 @@ def largestPopulation():
     largestPop = 0
 
     for row in row_list:
-        print(row[0])
         if(row[0] > int(largestPop)):
             largestPop = row[0]
     
@@ -49,6 +49,36 @@ def largestPopulation():
     print("City Name: ", row_list[0][0], ", City Population: ", row_list[0][1])
     
     return None
+
+def smallestPopulationMN():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dawsonj2",
+        user="dawsonj2",
+        password="eyebrow529redm")
+
+    cur = conn.cursor()
+
+    query = "SELECT city_population FROM cities"
+    cur.execute(query)
+
+    row_list = cur.fetchall()
+    smallestPop = sys.maxsize
+
+    for row in row_list:
+        if(row[0] < smallestPop):
+            smallestPop = row[0]
+
+    query = "SELECT city_name, city_population FROM cities WHERE city_population = %s"
+
+    cur.execute(query, [smallestPop])
+    row_list = cur.fetchall()
+
+    print("City Name: ", row_list[0][0], ", City Population: ", row_list[0][1])
+
+
+
 
 def main():
     isNorthfield()
