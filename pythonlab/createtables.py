51e49401c79d66cdd4b47982c03d58ba49c932ad
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-01-26 20:47:48 -0600

    Update createtables.py

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,182 +1,172 @@
+# We use the psycopg2 library to help us connec to the Postgres SQL database
+# This library is already installed on stearns.mathcs.carleton.edu
 import psycopg2
-import sys
+from config import config
 
 
-def isNorthfield():
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    
+
+    command1 = """CREATE TABLE cities (
+            city_name VARCHAR(255) NOT NULL,
+            city_state VARCHAR(255) NOT NULL,
+            city_population INTEGER NOT NULL,
+            city_lat REAL NOT NULL,
+            city_lon REAL NOT NULL
+        )"""
+
+    command2 = """CREATE TABLE states(
+            state_name VARCHAR(255) NOT NULL,
+            state_abb VARCHAR(255) NOT NULL
+        )"""
+        
+    
+    
+    conn = None
+    try:
+        # read the connection parameters
+        params = config()
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        # create table one by one
+        cur.execute(command1)
+        cur.execute(command2)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+    
+# This function tests to make sure that you can connect to the database
+def test_connection():
+
     conn = psycopg2.connect(
         host="localhost",
         port=5432,
         database="dawsonj2",
         user="dawsonj2",
-        password="eyebrow529redm")
+        password="Eyebrow529redm")
+
+    if conn is not None:
+        print( "Connection Worked!" )
+    else:
+        print( "Problem with Connection" )
+
+    return None
+
+
+# This function sends an SQL query to the database
+def test_query_one():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dawsonj2",
+        user="dawsonj2",
+        password="Eyebrow529redm")
 
     cur = conn.cursor()
 
-    query = "SELECT city_name FROM cities WHERE city_name = 'Northfield'"
-
-    cur.execute(query)
-    row = cur.fetchall()
-
-    if len(row) == 0:
-        row = "Northfield could not be found in the database"
+    sql = "SELECT name, abb FROM states WHERE abb = 'MN' "
     
-    print(row)
+    cur.execute( sql )
+
+    # fetchone() returns one row that matches your quer
+    row = cur.fetchone()
+
+    # Note: We could access individual items in the row
+    # That is, row[0] would be the name column in the previous example
+    #   ... and row[1] would be the abb column
+
+    #IMPORTANT: This function doesn't actually change the database
+    #If we are trying to change the database ...
+    # ... for example, creating a table
+    #Then we need the following command to finalize our changes
+
+    conn.commit()
+    
+    return row
+
+
+
+# This function sends a query that returns many items
+def test_query_all():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dawsonj2",
+        user="dawsonj2",
+        password="Eyebrow529redm")
+
+    cur = conn.cursor()
+
+    sql = "SELECT name, abb FROM states"
+    
+    cur.execute( sql )
+
+    # fetchall() returns a list containing all rows that matches your query
+    row_list = cur.fetchall()
+
+    # It is often useful to loop through all rows in a query result
+    for row in row_list:
+        print( row[1] )
+    
+    # Note: We could access individual items in the row
+    # That is, row[0] would be the name column in the previous example
+    #   ... and row[1] would be the abb column
+
+    # Here I am leaving out the conn.commit() because we aren't changing
+    #    either the database or the data in the database
     
     return None
 
-def largestPopulation():
+
+#Often we want to put a Python variable into an SQL query
+def test_query_variable():
+
     conn = psycopg2.connect(
         host="localhost",
         port=5432,
         database="dawsonj2",
         user="dawsonj2",
-        password="eyebrow529redm")
+        password="Eyebrow529redm")
 
     cur = conn.cursor()
 
-    query = "SELECT city_population FROM cities"
-    cur.execute(query)
+
+    # Here the %s signals that we will replace this with a variable later
+    sql = "SELECT name, abb FROM states WHERE abb = %s OR abb = %s "
+
+    state_abb1 = 'MN'
+    state_abb2 = 'NM'
+    
+    cur.execute( sql, [state_abb1, state_abb2]  )
+
+    # IMPORTANT: We need a list of values for the second input to execute
+    #   ... Even if we are only inserting my variable, it must be in a list
+    # For example,  [state_abb1]
+    
     row_list = cur.fetchall()
-    largestPop = 0
 
     for row in row_list:
-        if(row[0] > int(largestPop)):
-            largestPop = row[0]
-    
-    query = "SELECT city_name, city_population FROM cities WHERE city_population = %s"
-    cur.execute(query, [largestPop])
-    row_list = cur.fetchall()
-    
-    print("City Name: ", row_list[0][0], ", City Population: ", row_list[0][1])
-    
+        print(row)
+
     return None
 
-def smallestPopulationMN():
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="dawsonj2",
-        user="dawsonj2",
-        password="eyebrow529redm")
+#print( test_query_one() )
+create_tables()
+# test_query_all()
 
-    cur = conn.cursor()
-
-    query = "SELECT city_population FROM cities WHERE city_state = 'Minnesota'"
-    cur.execute(query)
-
-    row_list = cur.fetchall()
-    smallestPop = sys.maxsize
-
-    for row in row_list:
-        if(row[0] < smallestPop):
-            smallestPop = row[0]
-
-    query = "SELECT city_name, city_population FROM cities WHERE city_population = %s"
-
-    cur.execute(query, [smallestPop])
-    row_list = cur.fetchall()
-
-    print("City Name: ", row_list[0][0], ", City Population: ", row_list[0][1])
-
-def furthestDir():
-    conn = psycopg2.connect(
-    host="localhost",
-    port=5432,
-    database="dawsonj2",
-    user="dawsonj2",
-    password="eyebrow529redm")
-    
-    cur = conn.cursor()
-
-    query = "SELECT city_lat, city_lon, city_name FROM cities"
-    cur.execute(query)
-    row_list = cur.fetchall()
-
-    furthestNorth = sys.float_info.min
-    furthestSouth = sys.float_info.max
-    furthestEast = -500
-    furthestWest = sys.float_info.max
-
-    northCity = ""
-    southCity = ""
-    eastCity = ""
-    westCity = ""
-    
-    # row[0] is lat (North & South) and row[1] is long (East & West)
-    for row in row_list:
-        if row[0] > furthestNorth:
-            furthestNorth = row[0]
-            northCity = row[2]
-        if(row[0] < furthestSouth):
-            furthestSouth = row[0]
-            southCity = row[2]
-        if(row[1] > furthestEast):
-            furthestEast = row[1]
-            eastCity = row[2]
-        if(row[1] < furthestWest):
-            furthestWest = row[1]
-            westCity = row[2]
-    
-    query = "SELECT city_name, city_lat, city_lon FROM cities WHERE city_name = %s"
-    cur.execute(query, [northCity])
-    northRow = cur.fetchall()
-
-    query = "SELECT city_name, city_lat, city_lon FROM cities WHERE city_name = %s"
-    cur.execute(query, [southCity])
-    southRow = cur.fetchall()
-
-    
-    query = "SELECT city_name, city_lat, city_lon FROM cities WHERE city_name = %s"
-    cur.execute(query, [eastCity])
-    eastRow = cur.fetchall()
-    #print("City Name 2.0: ", eastRow[0][0])
-    
-
-    query = "SELECT city_name, city_lat, city_lon FROM cities WHERE city_name = %s"
-    cur.execute(query, [westCity])
-    westRow = cur.fetchall()
-
-    print(" Furthest City North: ", northRow[0][0], " (", northRow[0][1], ", ", northRow[0][2], ")\n",
-         "Furthest City South: ", southRow[0][0], " (", southRow[0][1], ", ", southRow[0][2], ")\n",
-         "Furthest City East: ", eastRow[0][0], " (", eastRow[0][1], ", ", eastRow[0][2], ")\n",
-         "Furthest City West: ", westRow[0][0], " (", westRow[0][1], ", ", westRow[0][2], ")")
-
-    
-
-
-def findPopulation():
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="dawsonj2",
-        user="dawsonj2",
-        password="eyebrow529redm")
-
-    cur = conn.cursor()
-    user = input("Enter State: ")
-
-    query = "SELECT city_name, city_population FROM cities WHERE city_state = %s"
-    cur.execute(query, [user])
-    row_list = cur.fetchall()
-
-    for row in row_list:
-        print("City: ", row[0], " Population: ", row[1])
-
-
-
-
-
-
-
-
-def main():
-    isNorthfield()
-    largestPopulation()
-    smallestPopulationMN()
-    furthestDir()
-    findPopulation()
-
-main()
-
+# test_query_variable()
 
 
