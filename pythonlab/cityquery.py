589a5c4b62ca226ce5c9a6780f935ef4fe0593fa
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-01-26 19:21:45 -0600

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -103,8 +103,7 @@ def furthestDir():
     westCity = ""
 
     for row in row_list:
-        row[1]
-        print("Testing")
+        row[0]
     # row[0] is lat (North & South) and row[1] is long (East & West)
     for row in row_list:
         if row[0] > furthestNorth:
