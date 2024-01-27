fe0d7f28a3a12939d70d59c97a6291855787eed2
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-01-26 18:50:35 -0600

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -138,7 +138,7 @@ def furthestDir():
     print(" Furthest City North: ", northRow[0][0], " (", northRow[0][1], ", ", northRow[0][2], ")\n",
          "Furthest City South: ", southRow[0][0], " (", southRow[0][1], ", ", southRow[0][2], ")\n",
          "Furthest City East: ", eastRow[0][0], " (", eastRow[0][1], ", ", eastRow[0][2], ")\n",
-         "Furthest City West: ", westRow[0][0], " (", westRow[0][1], ", ", westRow[0][2], ")\n")
+         "Furthest City West: ", westRow[0][0], " (", westRow[0][1], ", ", westRow[0][2])
 
     
 
