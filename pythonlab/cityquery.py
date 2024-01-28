08b061884946addeda34a445023153bf5033899d
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-01-27 20:10:49 -0600

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -19,7 +19,7 @@ def isNorthfield():
 
     print("Northfield Length: ", len(row))
     if len(row) == 0:
-        row = "Northfield could not be found in the database"
+        print = "Northfield could not be found in the database"
     else:
         print("Northfield (", row[0][1], ", ", row[0][2], ")")
 
