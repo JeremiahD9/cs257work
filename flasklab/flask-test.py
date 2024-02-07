ba04d27f68d3be4075543ce5f8afff6fc464e3af
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-02-07 09:08:17 -0600

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -18,10 +18,10 @@ def my_color(word1):
 
 @app.route('/add/<num1>/<num2>')
 def addition(num1, num2):
-    num1 = int(num1)
-    num2 = int(num2)
+    num1 = float(num1)
+    num2 = float(num2)
     addNum = num1 + num2
-    the_string = num1 + "+" + num2 + "=" + addNum
+    the_string = "" + num1 + "+" + num2 + "=" + addNum
     return the_string
 
 if __name__ == '__main__':
