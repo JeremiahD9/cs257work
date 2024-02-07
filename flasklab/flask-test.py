f54e6acfa917dc675c90da0be4350a240b35db1c
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-02-07 08:43:18 -0600

    Update flask-test.py

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -17,6 +17,6 @@ def my_color(word1):
     return '<h1 style="color:Red">' + word1 + '</h1>'
 
 if __name__ == '__main__':
-    my_port = ????
+    my_port = 5124
     app.run(host='0.0.0.0', port = my_port) 
 
