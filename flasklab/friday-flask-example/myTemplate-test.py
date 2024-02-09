aaa9204d0ba6c7d42236796d1f45d5b7c473c7cf
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-02-09 09:18:00 -0600

    Update myTemplate-test.py

--- flasklab/friday-flask-example/myTemplate-test.py
+++ flasklab/friday-flask-example/myTemplate-test.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template("index.html")
+    return render_template("SentenceGenerator.html")
 
 @app.route('/rand/<low>/<high>')
 def rand(low, high):
