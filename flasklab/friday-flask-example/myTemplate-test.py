af86d51d9ced82cddaf49d1cb4cbe53e1ddd5719
Jeremiah Dawson <153328521+JeremiahD9@users.noreply.github.com>
2024-02-09 23:07:18 -0600

    Update myTemplate-test.py

--- flasklab/friday-flask-example/myTemplate-test.py
+++ flasklab/friday-flask-example/myTemplate-test.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 import random
+import psycopg2
 
 app = Flask(__name__)
 
@@ -17,6 +18,37 @@ def rand(low, high):
     
     num = random.randint(low_int, high_int)
     return render_template("random.html", randNum = num)
+
+@app.route('/sentence')
+def randSentence():
+    names = ["Jeremiah", "Mason", "Kyle", "SJ", "Josh", "Makai", "Noa", "Noah", "Raghav", "Gus", "Mariana", "Alice", "Mary", "Shanti", "Ahana", "Kiana", "Katelyn", "Monica"]
+    adjectives = ["ugly", "cute", "sexy", "weird", "cool", "hot", "creepy", "musty"]
+    years = [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006]
+
+    name = random.choice(names)
+    adjective = random.choice(adjectives)
+    year = random.choice(years)
+
+    sentence = name + " the " + adjective + " was born in " + str(year)
+    return sentence
+
+
+def randCity():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dawsonj2",
+        user="dawsonj2",
+        password="eyebrow529redm")
+
+    cur = conn.cursor()
+    query = "SELECT * FROM cities ORDER BY RANDOM()"
+    cur.execute(query)
+
+    row_list = cur.fetchone()
+
+    return row_list[0][0]
+
 
 if __name__ == '__main__':
     my_port = 5124
